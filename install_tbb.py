4db5ab8d8340694a499c18d277e33ccfa2c9473b
pluk <Philip.luk@gmail.com>
2020-05-13 23:11:31 -0400

    Fix bugs with build and installation steps.  Utilize REZ_BUILD_INSTALL boolean to determine if an installation should occur.

--- install_tbb.py
+++ install_tbb.py
@@ -38,30 +38,32 @@ def copy_dir(src_dir, dst_dir):
     shutil.copytree(src_dir, dst_dir)
 
 if __name__ == '__main__':
-    cmake_args = ['cmake',
-                  '-g "Ninja"',
-                  '-DTBB_BUILD_SHARED=ON',
-                  '-DTBB_BUILD_TBBMALLOC=ON',
-                  '-DTBB_BUILD_TBBMALLOC_PROXY=ON',
-                  '-DTBB_BUILD_STATIC=ON',
-                  os.environ.get('REZ_BUILD_SOURCE_PATH'),
-                 ]
-    run_command(cmake_args, os.environ.get('REZ_BUILD_PATH'))
+    # Configure CMake.
+    cmake_cmd_list = [
+        'cmake',
+        '-g "Ninja"',
+        '-DTBB_BUILD_SHARED=ON',
+        '-DTBB_BUILD_TBBMALLOC=ON',
+        '-DTBB_BUILD_TBBMALLOC_PROXY=ON',
+        '-DTBB_BUILD_STATIC=ON',
+        os.environ.get('REZ_BUILD_SOURCE_PATH'),
+    ]
+    run_command(cmake_cmd_list, os.environ.get('REZ_BUILD_PATH'))
 
-    if 'REZ_BUILD_INSTALL_PATH' in os.environ:
-        cmake_args = ['cmake',
-                      '--install',
-                      '.',
-                      '--prefix',
-                      os.environ.get('REZ_BUILD_INSTALL_PATH')
-                      ]
-    else:
-        cmake_args = ['cmake',
-                      '--build',
-                      '.',
-                      ]
-    run_command(cmake_args, os.environ.get('REZ_BUILD_PATH'))
+    # Do the build.
+    cmake_cmd_list = ['cmake', '--build', '.']
+    run_command(cmake_cmd_list, os.environ.get('REZ_BUILD_PATH'))
 
-    if 'REZ_BUILD_INSTALL_PATH' in os.environ:
+    if bool(os.environ['REZ_BUILD_INSTALL']):
+        # Do the installation to the REZ repository.
+        cmake_cmd_list = [
+            'cmake',
+            '--install',
+            '.',
+            '--prefix',
+            os.environ.get('REZ_BUILD_INSTALL_PATH')
+        ]
+        run_command(cmake_cmd_list, os.environ.get('REZ_BUILD_PATH'))
+
         copy_dir(os.path.join(os.environ.get('REZ_BUILD_SOURCE_PATH'), 'include', 'serial'),
                  os.path.join(os.environ.get('REZ_BUILD_INSTALL_PATH'), 'include', 'serial'))
